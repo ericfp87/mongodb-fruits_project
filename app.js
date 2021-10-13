5efaddafc03c087bc9e45b16e4b34ddb4ce81035
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-10-13 12:11:57 -0300

    Mongoose

--- app.js
+++ app.js
@@ -1,54 +1,120 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-//Connection URL
-const url = 'mongodb://localhost:27017';
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
-//Database Name
-const dbName = 'fruitsDB';
-
-//Create a new MongoClient
-const client = new MongoClient(url, {useNewUrlParser: true});
-
-//Use connect method to connect to the server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Conectado no servidor");
-
-  const db = client.db(dbName);
-
-  insertDocuments(db, function(){
-    client.close();
-  });
-
+const fruitSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: [true, "Não foi especificado o nome"]
+  },
+  rating:{
+    type: Number,
+    min: 1,
+    max: 50
+  },
+  review: String,
 });
 
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const insertDocuments = function(db, callback) {
-  const collection = db.collection('fruits');
+const fruit = new Fruit ({
+  name: "Apple",
+  rating: 7,
+  review: "Linda maçã"
+});
 
-  collection.insertMany([
-    {
-      name: "Apple",
-      score: 8,
-      review: "Great fruit"
-    },
-    {
-      name: "Orange",
-      score: 6,
-      review: "Kinda sour"
-    },
-    {
-      name: "Banana",
-      score: 9,
-      review: "Great stuff!"
-    }
+// fruit.save();
 
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3,result.insertedCount);
-    assert.equal(3,Object.keys(result.insertedIds).length);
-    console.log("Inseridos 3 documentos");
-    callback(result);
-  });
-};
+const personSchema = new mongoose.Schema ({
+  name: String,
+  age: Number,
+  favouriteFruit: fruitSchema
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const morango = new Fruit ({
+  name: "Morango",
+  rating: 4,
+  review: "Vermelho"
+});
+
+// morango.save();
+
+const person = new Person ({
+  name: "Amy",
+  age: 20,
+  favouriteFruit: morango
+});
+
+// person.save();
+
+// const kiwi =  new Fruit ({
+//   name: "Kiwi",
+//   rating: 10,
+//   review: "O melhor"
+// });
+// const orange = new Fruit ({
+//   name: "Orange",
+//   rating: 12,
+//   review: "Laranja"
+// });
+// const banana = new Fruit ({
+//   name: "Banana",
+//   rating: 4,
+//   review: "Amarela"
+// });
+
+//inserir várias frutas
+// Fruit.insertMany([kiwi, orange, banana], function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Sucesso");
+//   }
+//
+// });
+
+
+Fruit.find(function(err, fruits){
+  if (err) {
+    console.log(err);
+  } else {
+    mongoose.connection.close();
+    // mostra todos os dados em fruits
+    // console.log(fruits);
+
+    //mostra apenas o nome das frutas
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
+    });
+  }
+})
+
+//atualizar algum dado
+// Fruit.updateOne({_id: "6166edc1162f7908c9edccab"}, {name: "Pessego"}, function(err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("Sucesso");
+//   }
+// });
+
+
+//Deletar
+// Fruit.deleteOne({name: "Pessego"}, function(err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("Deletado");
+//   }
+// });
+
+//Deletar muitos dados
+// Person.deleteMany({name: "John"}, function(err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("Deletados");
+//   }
+// });
